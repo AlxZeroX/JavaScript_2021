b1e6686c8a0758d15b7e8e6a0e8ec50a64c76cd5
Cristian_Martinez <thealxzerox@gmail.com>
2021-11-23 17:30:41 -0600

    bprac

--- Tipos de Datos en JavaScript/BPractice.js
+++ Tipos de Datos en JavaScript/BPractice.js
@@ -6,3 +6,8 @@ x=10, y=20;
 let z = x+y;
 console.log(z);
 
+//Definicion de Variables
+let a1nombreCompleto;
+let $1nombreCompleto;
+//let 1nombreCompleto; no se permite iniciar variables con numeros
+
