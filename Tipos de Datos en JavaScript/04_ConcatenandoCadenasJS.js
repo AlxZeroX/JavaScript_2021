c834795b27f5fe7592de59aeb3220800525f609a
Cristian_Martinez <thealxzerox@gmail.com>
2021-11-23 17:14:27 -0600

    conJS

---
+++ Tipos de Datos en JavaScript/04_ConcatenandoCadenasJS.js
@@ -0,0 +1,22 @@
+var nombre = "Alex";
+var apellido ="Martinez";
+
+//Por variables
+var NombreCompleto = nombre + " " + apellido;
+console.log(NombreCompleto);
+
+//Por string
+var NombreCompleto2 = 'Cris' + ' ' + ' Martinez';
+console.log(NombreCompleto2);
+
+//variable + numero(string)
+var x = nombre + 2 + 2;
+console.log(x);
+
+//variable + numero
+var x = nombre + (2 + 2);
+console.log(x);
+
+//numero + variable
+x = 2 + 2 + nombre;
+console.log(x);
