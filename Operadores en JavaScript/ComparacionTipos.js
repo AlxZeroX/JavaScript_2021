cb49c64e393657f8b29e61ef18f7561b8a40160e
Cristian_Martinez <thealxzerox@gmail.com>
2021-11-23 18:44:31 -0600

    up

--- Operadores en JavaScript/ComparacionTipos.js
+++ Operadores en JavaScript/ComparacionTipos.js
@@ -1,7 +1,13 @@
 let a=3, b=2, c="3";
 
 let z = a == c; //Se revisa el valor sin importar el tipo
-console.log(z)
+console.log(z);
 
 z = a ===c;//Revisa los valores y los tipos
-console.log(z)
+console.log(z);
+
+z = a != c; //Se revisa el valor sin importar el tipo
+console.log(z);
+
+z = a !==c;//Revisa los valores y los tipos
+console.log(z);
